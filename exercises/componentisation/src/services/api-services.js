9536b85b1e4d38424989b5bc516b4a3a107f9e10
Léo Floissac <leofloissac@hotmail.com>
2026-03-04 13:23:11 +0100

    refactorisation  done for componentisation

---
+++ exercises/componentisation/src/services/api-services.js
@@ -0,0 +1,42 @@
+class ApiService {
+    async get(path) {
+      try {
+        const response = await fetch(`/api${path}`, {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json', Authorization: `JWT ${this.token}` },
+        });
+        return await response.json();
+      } catch (error) {
+        throw error;
+      }
+    }
+  
+    async post(path, body) {
+      try {
+        const response = await fetch(`/api${path}`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json', Authorization: `JWT ${this.token}` },
+          body: JSON.stringify(body),
+        });
+        return await response.json();
+      } catch (error) {
+        throw error;
+      }
+    }
+  
+    async put(path, body) {
+      try {
+        const response = await fetch(`/api${path}`, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json', Authorization: `JWT ${this.token}` },
+          body: JSON.stringify(body),
+        });
+        return await response.json();
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
+  
+  const api = new ApiService();
+  export default  api;
